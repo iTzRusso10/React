bf87bc7649eaeaa3281bc320d6a548fc522e85c1
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-01-27 15:05:52 +0100

    Exercise-10 Props-01

---
+++ react-exercise/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Welcome } from "./Welcome";
+
+export class App extends React.Component{
+    render(){
+        return(
+            <Welcome name="Giuseppe"/>
+        )
+    }
+}
