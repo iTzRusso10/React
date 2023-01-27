2f61fa8f76542946660c576e9e23a276345f2dbd
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-01-27 01:45:39 +0100

    Exercise-02 JSX-02

--- react-exercise/src/index.js
+++ react-exercise/src/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+function getName(name){
+  return <h1>Hello, {name}</h1>
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(getName("Giuseppe! 🟢"), document.getElementById(`root`))
+export default getName;
