27c3aaf10df5aea96157696e75f2aa0d73beea9a
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-01-27 08:16:46 +0100

    Exercise-07 Components-04

---
+++ react-exercise/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Hello } from "./Hello";
+
+export class App extends React.Component{
+    render(){
+        return(
+            <Hello />
+        )
+    }
+}
