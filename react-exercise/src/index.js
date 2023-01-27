be81583ebc49351d9370372e9174072161781be0
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-01-27 02:01:25 +0100

    Exercise-03 JSX-03

--- react-exercise/src/index.js
+++ react-exercise/src/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root')
+const sum = (a,b) => {
+  return<h2>The sum is {a + b}</h2>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(sum(12,22), root)
