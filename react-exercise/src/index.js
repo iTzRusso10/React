37fd93ebe6115f031ed0f6b0a578340b933c482b
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-01-27 01:36:17 +0100

    Exercise-01 JSX-01

--- react-exercise/src/index.js
+++ react-exercise/src/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const hello = <h1>Hello, World!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(hello);
