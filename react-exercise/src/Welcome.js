45d67c4f7964ee3e721b7c3507cdc66aea05a09c
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-01-28 17:42:14 +0100

    Exercise-11 Props-02

---
+++ react-exercise/src/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Welcome extends React.Component{
+    render(){
+        return(
+            <div>
+            {!!this.props.name && <h1>Welcome, {this.props.name}</h1>}
+            {!this.props.name && <h1>Welcome, User!</h1>}
+            </div>
+        )
+    }
+}
