e70018198dc122925fb89b29a651df57966e90ce
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-02-02 16:53:48 +0100

    Exercise-10 Conditional-Rendering-05

---
+++ react-exercise/src/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Age } from "./Age";
+
+export class Welcome extends React.Component{
+    render(){
+        return(
+            <div>
+            {!!this.props.name && <h1>Welcome, {this.props.name}</h1>}
+            {!this.props.name && <h1>Welcome, User!</h1>}
+            {this.props.age > 18 && <Age />}
+            {this.props.age < 18 && <p>You are very young!</p>}
+            </div>
+        )
+    }
+}
