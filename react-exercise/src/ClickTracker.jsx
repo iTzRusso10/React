9c552ef86547e72acea728d3cd16b629ac2ed47a
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-02-04 16:33:39 +0100

    Exercise-25 Event-02

---
+++ react-exercise/src/ClickTracker.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+class ClickTracker extends Component{
+
+    state = {
+        target : null
+    }
+    
+    tracker = (event) => {
+        this.setState(  {
+            target : event.target.innerHTML
+        })
+        console.log(event.target.innerHTML)
+    }
+
+    button = document.getElementById(`a`);
+
+    render(){
+        return(
+            <>
+            <button onClick={this.tracker}>Click 1</button>
+            <button onClick={this.tracker}>Click 2</button>
+            <button onClick={this.tracker} name = "Click 3">Click 3</button>
+            <h1>Il bottone cliccato è : {this.state.target} </h1>
+            </>
+        )
+    }
+}
+
+
+export default ClickTracker;
--- react-exercise/src/index.js
+++ react-exercise/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ClickTracker from './ClickTracker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<ClickTracker />)
