853044717a133390665af943dfc19696590017d6
Giuseppe Russo <giusepperusso1003@gmail.com>
2023-02-06 13:22:36 +0100

    Exercise-28 Form-03

--- react-exercise/src/index.js
+++ react-exercise/src/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const login = () => {
+    alert("Sei loggato")
+}
+
+
+root.render(<Login onLogin={login}/>)
