89cfec06b692155d2d4ba52aa6d7ac385f871bde
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-09 10:35:32 +0100

    Exercise-36 Styling-Component-02

---
+++ react-exercise/src/Login.jsx
@@ -0,0 +1,41 @@
+import { Component } from "react";
+
+class Login extends Component {
+    state = {
+        username : "",
+        password : "",
+        remember : false
+    }
+    
+    trackEvent = (event) => {
+       const name = event.target.name;
+       const type = event.target.type;
+       const value = event.target.value;
+       const checked = event.target.checked;
+
+       this.setState({
+        [name]: type === "checkbox" ? checked : value
+       })
+    }
+
+    componentDidUpdate(){
+        console.log(this.state)
+    }
+
+
+    render(){
+        const MyStyle = {
+            backgroundColor : this.state.password.length < 8 ? `red`  : `green`
+        }
+        return(
+            <div>
+                <input name="username" value={this.state.username} onChange={this.trackEvent}/>
+                <input name="password" value={this.state.password} type="password" onChange={this.trackEvent}/>
+                <input name="remember" value={this.state.remember} type="checkbox" onChange={this.trackEvent}/>
+                <button disabled={!this.state.username || !this.state.password} onClick={this.props.onLogin} style={MyStyle}>Login</button>
+            </div>
+        )
+    }
+}
+
+export default Login;
--- react-exercise/src/index.js
+++ react-exercise/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Welcome} from './Welcome';
-import './index.css';
+import  Login  from './Login'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Welcome name="Giuseppe" age="20"/>);
+root.render(<Login />);
