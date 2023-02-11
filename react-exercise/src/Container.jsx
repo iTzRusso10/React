af149dedbfbff8474b4934e54c5f058d3f433de9
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-11 17:35:59 +0100

    Exercise-37 Component-Composition-1

---
+++ react-exercise/src/Container.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Container extends React.Component {
+    render() {
+        const myStyle = {
+            backgroundColor: "white",
+            border: "1px solid red",
+        }
+        return (
+            <div style={myStyle}>
+                {this.props.children}
+            </div>
+        ); 
+    }
+}
+
+export default Container;
