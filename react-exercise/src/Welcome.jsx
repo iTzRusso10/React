b4836cfc3c5d27e1e2eaa8f9476f3bf8ee65ba68
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-15 20:13:23 +0100

    Exercise-42 Functions-Component

---
+++ react-exercise/src/Welcome.jsx
@@ -0,0 +1,9 @@
+
+export function Welcome({name, age}) {
+    return(
+        <div>
+            <h1>Ciao, {name}</h1>
+            <p>Hai {age} anni</p>
+        </div>
+    )
+}
--- react-exercise/src/index.js
+++ react-exercise/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+
+import { Welcome } from './Welcome';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<Welcome name="Giuseppe" age={20}/>);
