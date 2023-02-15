62d43703679a699767ea4cf824f45f337148b695
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-15 20:21:38 +0100

    Exercise-44 Functions-COmponent-3

--- react-exercise/src/Sum.js
+++ react-exercise/src/Sum.js
@@ -1,5 +1,5 @@
-export function Sum({numbers}){
-    numbers = [10,20]
+export function Sum({numbers = [0]}){
+    // numbers = [10,20]
     const sum = numbers.reduce((a,b) => a + b);
         return(
         <h1>The sum is: {sum}</h1>
