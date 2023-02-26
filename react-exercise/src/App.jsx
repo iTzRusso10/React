612dbde25102dd6a69f62240f32e9297983d08b7
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-26 21:53:55 +0100

    Exercise-60 Router-02

--- react-exercise/src/App.jsx
+++ react-exercise/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
+import Counter from "./Counter";
 import Welcome from "./Welcome"
 
 const App = () => {
     return(
         <Routes>
             <Route path="/" element={<Welcome name="Giuseppe"/>}></Route>
+            <Route path="/counter" element={<Counter />}></Route>
         </Routes>
     )
 }
