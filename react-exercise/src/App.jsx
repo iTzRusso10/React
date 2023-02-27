ce047f4e6361c9494286e5c41dabd10c4f2ae4ac
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-27 06:31:58 +0100

    Exercise-64 Router-06

--- react-exercise/src/App.jsx
+++ react-exercise/src/App.jsx
@@ -1,19 +1,23 @@
-import { Link, Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom";
 import GithubUser from "./GithubUser";
 import NotFound from "./NotFound";
-import Welcome from "./Welcome"
+import Welcome from "./Welcome";
 
 const App = () => {
-    return(
-        <>
-        <Routes>
-            <Route path="/home" element={<Welcome name="Giuseppe"/>}></Route>
-            <Route path='/:username' element={<GithubUser/>}></Route>
-            <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-        <Link to='/username'>GitHub User</Link> | <Link to='/home'>Torna alla home</Link> | <Link to="/home/contact">Contatti</Link>
-        </>
-    )
-}
+  return (
+    <>
+      <Routes>
+        <Route path="/home" element={<Welcome name="Giuseppe" />}></Route>
+        <Route path="/users" element={<GithubUser />}>
+          <Route path=":username" element={<GithubUser />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      <Link to="/users">GitHub User</Link> |{" "}
+      <Link to="/home">Torna alla home</Link> |{" "}
+      <Link to="/home/contact">Contatti</Link>
+    </>
+  );
+};
 
 export default App;
