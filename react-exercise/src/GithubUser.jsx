1822570f61dffb786313950fe908a22791072b58
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-27 09:51:30 +0100

    Exercise-69 SWR-03

--- react-exercise/src/GithubUser.jsx
+++ react-exercise/src/GithubUser.jsx
@@ -1,11 +1,14 @@
 import { useParams } from 'react-router-dom'
-import useSWR from 'swr'
+import useSWR , { mutate } from 'swr'
 
 const fetcher = async (url) => await(await (fetch(url))).json()
 const GithubUser = () => {
 const {username} = useParams();
 const { data, error } = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
 
+const Refresh = async () => {
+    mutate()
+}
 return(
     <div>
         {!data && !error && <h1>Caricamento...</h1>}
@@ -13,7 +16,9 @@ return(
         {data && !error && <ul>
             <li key={Math.random()}>{data.login}</li>
             <li key={Math.random()}>{data.id}</li>
+            <li key={Math.random()}>{data.bio}</li>
             </ul>}
+            <button onClick={Refresh}>Aggiorna</button>
     </div>
 )
 }
