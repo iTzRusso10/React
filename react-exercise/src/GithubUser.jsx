dbb3b604319b00050d945ccb4d762005ce55c373
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-27 06:02:54 +0100

    Exercise-61 Router-03

---
+++ react-exercise/src/GithubUser.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom";
+
+const GithubUser = () => {
+
+const [user,setUser] = useState('')
+const [load,setLoad] = useState(false)
+const {username} = useParams();
+
+useEffect(() => {
+    setLoad(true);
+    const getUser = async () => {
+        const {name,id,login,avatar_url} = await (await fetch(`https://api.github.com/users/${username}`)).json();
+        setUser({name,id,login,avatar_url});
+        setLoad(false);
+    }
+    getUser()
+    
+},[username])
+;
+
+    return(
+        <div>
+            {load && <h1>Caricamento...</h1>}
+            <h2>{user.name}</h2>
+            <img src={user.avatar_url} alt="ProfileImage" style={{width :"200px"}}></img>
+            <p>{user.id}</p>
+            <p>{user.login}</p>
+            <Link to='/'>Torna alla home</Link>
+        </div>
+    )
+}
+
+export default GithubUser;
