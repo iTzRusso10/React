abb20d95d346d4cc2cb140fca6f0f4a07b20f507
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-27 06:22:36 +0100

    Exercise-63 Router-05

--- react-exercise/src/App.jsx
+++ react-exercise/src/App.jsx
@@ -1,15 +1,17 @@
 import { Link, Route, Routes } from "react-router-dom"
 import GithubUser from "./GithubUser";
+import NotFound from "./NotFound";
 import Welcome from "./Welcome"
 
 const App = () => {
     return(
         <>
         <Routes>
-            <Route path="/" element={<Welcome name="Giuseppe"/>}></Route>
+            <Route path="/home" element={<Welcome name="Giuseppe"/>}></Route>
             <Route path='/:username' element={<GithubUser/>}></Route>
+            <Route path="*" element={<NotFound />}></Route>
         </Routes>
-        <Link to='/username'>GitHub User</Link> | <Link to='/'>Torna alla home</Link>
+        <Link to='/username'>GitHub User</Link> | <Link to='/home'>Torna alla home</Link> | <Link to="/home/contact">Contatti</Link>
         </>
     )
 }
---
+++ react-exercise/src/NotFound.jsx
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+    return(
+        <>
+        <h1 style={{textAlign: "center"}}>Oops, non abbiamo trovato nulla.</h1>
+        </>
+    )
+}
+
+export default NotFound;
